9a02c92d4f99cab93cebb340a6ea132466c1958e
sanjaykumar0324 <114232171+sanjaykumar0324@users.noreply.github.com>
2024-06-23 20:13:08 +0530

    Update cartModel.js

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,23 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const cartSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Users",
-    required: [true, "User is required"]
+    ref: 'Users',
+    required: [true, 'User is required']
   },
   items: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "CartItem"
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'products',
+        required: [true, 'Product is required']
+      },
+      quantity: {
+        type: Number,
+        required: [true, 'Quantity is required'],
+        default: 1,
+        min: 1,
+        max: 10
+      }
     }
   ],
   price: {
     type: Number,
-    required: [true, "Price is required"],
+    required: [true, 'Price is required'],
     default: 0
   }
 }, { timestamps: true });
 
-const Cart = mongoose.model("Cart", cartSchema);
+const Cart = mongoose.model('Cart', cartSchema);
 export default Cart;
